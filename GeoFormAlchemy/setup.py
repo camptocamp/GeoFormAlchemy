f2cbb9a537a2fe5c8e3dc2bda849d4d81a53cfb6
Éric Lemoine <eric.lemoine@gmail.com>
2011-01-10 11:32:43 +0100

    change GeoAlchemy requirement to >=0.4,<=0.4.99

--- GeoFormAlchemy/setup.py
+++ GeoFormAlchemy/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1.1'
+version = '0.2'
 
 setup(name='GeoFormAlchemy',
       version=version,
@@ -29,7 +29,7 @@ setup(name='GeoFormAlchemy',
       zip_safe=True,
       install_requires=[
             'FormAlchemy>=1.3.0,<=1.3.99',
-            'GeoAlchemy>=0.3,<=0.3.99'
+            'GeoAlchemy>=0.4,<=0.4.99'
       ],
       entry_points="""
        [paste.paster_create_template]
